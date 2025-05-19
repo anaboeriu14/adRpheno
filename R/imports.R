944b06f4e3aeca99a8593c88a4460d14b7791200
anaboeriu14 <ilincaboeriu@gmail.com>
2025-05-19 13:51:58 -0700

    minor fix

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,7 @@
 #' @description This file contains all package imports and namespace configurations.
 #' @name adRpheno
 #'
-#' @import adRutils is_processed register_processed reset_processing
+#' @importFrom adRutils is_processed register_processed reset_processing
 #' @importFrom dplyr %>% arrange bind_rows filter group_by if_else mutate rename rowwise select summarize ungroup
 #' @importFrom stringr str_extract str_remove str_replace str_split
 #' @importFrom tidyr pivot_longer pivot_wider replace_na
